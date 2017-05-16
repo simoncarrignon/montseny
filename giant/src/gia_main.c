5a26a4b2ee4d094fadf617bf7105a059a91f4340
GG1991 <giuntoli1991@gmail.com>
2017-05-16 14:18:11 +0200

    correcting printing messages on gia_main.c

--- giant/src/gia_main.c
+++ giant/src/gia_main.c
@@ -13,15 +13,15 @@ static char help[] = "Solves an RVE problem.\n\n";
 int main(int argc, char **args)
 {
 
-    char       mesh_n[16];
-    char       mesh_f[16];
+    char       mesh_n[64];
+    char       mesh_f[4];
 
     PetscBool  set;
     
 
     ierr = PetscInitialize(&argc,&args,(char*)0,help);
 
-    /* read options from command line */
+    // read options from command line 
     //    
     // couple flag ( 1:coupled 0:not coupled) -> -c 1
     //
@@ -29,7 +29,7 @@ int main(int argc, char **args)
     if(set == PETSC_FALSE){
 	couple_fl = PETSC_FALSE;
     }
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"coupling : %d\n",couple_fl);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"coupling    : %d\n",couple_fl);CHKERRQ(ierr);
     //
     // mesh file format  -> -mfor gmsh
     //
@@ -43,10 +43,13 @@ int main(int argc, char **args)
     //
     ierr = PetscOptionsGetString(NULL,NULL,"-mesh",mesh_n,16,&set);CHKERRQ(ierr);
     if(set == PETSC_FALSE){
-	strcpy(mesh_f,"rve/cube_unif/cube.msh");
+	strcpy(mesh_n,"rve/cube_unif/cube.msh");
     }
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"mesh file   : %s\n",mesh_n);CHKERRQ(ierr);
     //
 
+    // read mesh
+    //    
     ierr = gia_rmsh(mesh_n, mesh_f);
 
     ierr = PetscFinalize();
