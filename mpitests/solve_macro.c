52c211e1ddf5ceec0e310c81524a388521c68323
Guido Giuntoli <giuntoli1991@gmail.com>
2017-05-20 11:29:40 +0200

    interconection seems to work

--- mpitests/solve_macro.c
+++ mpitests/solve_macro.c
@@ -8,8 +8,13 @@ int solve_macro(MPI_Comm macroworld, MPI_Comm intercomm)
     int       rank;
     int       size;
 
+    int       nval = 6;
+    double    values[6] = { 0.1 ,0.2, 0.4, 0.0, -1.0, 6.0};
+
     MPI_Comm_size(macroworld, &size);
     MPI_Comm_rank(macroworld, &rank);
+
+    MPI_Send(values,nval,MPI_DOUBLE,0,0,intercomm);
 
     printf("I am rank %d of %d i call from %s\n",rank,size,"solve_macro");
 
--- mpitests/solve_micro.c
+++ mpitests/solve_micro.c
@@ -5,13 +5,23 @@
 int solve_micro(MPI_Comm microworld, MPI_Comm intercomm)
 {
 
-    int       rank;
-    int       size;
+    int        rank;
+    int        size;
+
+    int        nval=6;
+    double     values[] = { 0.0 ,0.0, 0.0, 0.0, 0.0, 0.0};
+
+    MPI_Status status;
 
     MPI_Comm_size(microworld, &size);
     MPI_Comm_rank(microworld, &rank);
 
-    printf("I am rank %d of %d i call from %s\n",rank,size,"solve_micro");
+    printf("micro rank %d of %d i call from %s\n values %lf %lf %lf %lf %lf %lf \n",rank,size,"solve_micro",values[0],values[1],values[2],values[3],values[4],values[5]);
+
+    if(rank==0)
+	MPI_Recv(values,nval,MPI_DOUBLE,0,0,intercomm,&status);
+
+    printf("micro rank %d of %d i call from %s\n values %lf %lf %lf %lf %lf %lf \n",rank,size,"solve_micro",values[0],values[1],values[2],values[3],values[4],values[5]);
 
     return 0;
 }
