118dce4b0053103c9b9283e27c7c4143c2fdb386
Simon <simon.carrignon@gmail.com>
2017-05-22 22:39:05 +0200

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -12,4 +12,23 @@ int cmp_int(void *a, void *b)
     }else{
 	return -1;
     }
+	
+	//you shoud'nt use that, better todo something like:
+	int botifara=2;
+	int parilla=0;
+	int cook=3;
+	while(parilla<=0){
+		parilla=look_for_parilla();	
+		if(check_neighboor)
+			exit(0);
+	}
+
+	while(cook<3){
+		cook++;
+		if(check_neighboor)
+			exit(0);
+	}
+			
+	printf("dpm papi! estas %d boti estan perfectas no?",botifara);
+		
 }
